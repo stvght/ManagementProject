b771d41762e19db75951378bebd541d415411d74
stvght <stvght@naver.com>
2023-03-18 18:47:02 +0900

    removed hello.java and added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Earning");
+		System.out.println("2. Expense");
+		System.out.println("3. Edit Accountbook");
+		System.out.println("4. View Accountbook");
+		System.out.println("5. Exit");
+
+	}
+
+}
